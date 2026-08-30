4e482d2078627998c7b084be21d5973276f6042d
trcya <davidflashs34@gmail.com>
2026-08-30 13:28:00 +0700

    add sticky note for tutorial channels

---
+++ events/stickyNote.js
@@ -0,0 +1,33 @@
+const { Events } = require('discord.js');
+
+const STICKY_CHANNELS = {
+    '1494149400052633671': {
+        content: 'Tutorial <@1543492628866400358>'
+    },
+    '1494149497360617553': {
+        content: 'Tutorial <@1543492628866400358>'
+    }
+};
+
+const lastStickyMessage = new Map();
+
+module.exports = {
+    name: Events.MessageCreate,
+    async execute(message) {
+        if (message.author.bot || !message.guild) return;
+
+        const sticky = STICKY_CHANNELS[message.channel.id];
+        if (!sticky) return;
+
+        try {
+            const oldMsgId = lastStickyMessage.get(message.channel.id);
+            if (oldMsgId) {
+                const oldMsg = await message.channel.messages.fetch(oldMsgId).catch(() => null);
+                if (oldMsg) await oldMsg.delete().catch(() => {});
+            }
+
+            const newMsg = await message.channel.send({ content: sticky.content });
+            lastStickyMessage.set(message.channel.id, newMsg.id);
+        } catch {}
+    },
+};
